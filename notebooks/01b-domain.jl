2b6b4e5ff2de494f3888e37c1b6a7c558bda6fee
natgeo-wong <natgeo.wong@outlook.com>
2023-10-31 17:55:19 -0400

    Changing domain notebook

--- notebooks/01b-domain.jl
+++ notebooks/01b-domain.jl
@@ -92,8 +92,8 @@ end
 
 # ╔═╡ 60d38ccd-6538-426f-985e-9d782834dce9
 begin
-	xinset = deepcopy(x); xinset[(x.>285).|(x.<270).|(y.>15).|(y.<0)].=NaN
-	yinset = deepcopy(y); yinset[(x.>285).|(x.<270).|(y.>15).|(y.<0)].=NaN
+	xinset = deepcopy(x); xinset[(x.>305).|(x.<250).|(y.>35).|(y.<-20)].=NaN
+	yinset = deepcopy(y); yinset[(x.>305).|(x.<250).|(y.>35).|(y.<-20)].=NaN
 	md"Filtering out coastlines outside domain ..."
 end
 
@@ -129,17 +129,18 @@ begin
 
 	lvls = vcat(10. .^(-5:0.5:-1))
 	lvls = vcat(lvls,0.25,0.5,0.9,0.95,0.99)
+	lvls = -6000 : 1000 : 6000
 
-	cmap_dict = Dict("right"=>0.65)
+	cmap_dict = Dict()
 	
 	axs[1].pcolormesh(
-		lsd_large.lon,lsd_large.lat,lsd_large.lsm',alpha=0.6,
+		lsd_large.lon,lsd_large.lat,lsd_large.z',alpha=0.3,
 		levels=lvls,cmap="delta",extend="both",cmap_kw=cmap_dict
 	)
 	
 	c = axs[1].pcolormesh(
-		lsd.lon,lsd.lat,lsd.lsm',
-		levels=lvls,cmap="delta",extend="both",cmap_kw=cmap_dict
+		lsd.lon,lsd.lat,lsd.z'/1000,
+		levels=lvls./1000,cmap="delta",extend="both",cmap_kw=cmap_dict
 	)
 	
 	axs[1].plot(xinset,yinset,lw=0.5,c="k")
@@ -149,18 +150,17 @@ begin
 	axs[1].plot([0,3,3,0,0].+275,[0,0,2.5,2.5,0].+8,lw=1,c="k",linestyle="--")
 	axs[1].plot([275,288],[10.5,15],lw=1,c="k",linestyle=":",zorder=3)
 	axs[1].plot([278,295],[8,9.1],lw=1,c="k",linestyle=":",zorder=3)
-	axs[1].plot([270,285,285,270,270],[0,0,15,15,0],lw=5,c="w")
+	axs[1].plot([270,285,285,270,270],[0,0,15,15,0],lw=5,c="k")
 	axs[1].plot(slon_PAC,slat_PAC,lw=2,)
 	axs[1].plot(slon_ATL,slat_ATL,lw=2,)
 	axs[1].plot(slon_PCS,slat_PCS,lw=2,)
 	axs[1].text(271.5,1.5,"PAC",bbox=Dict("facecolor"=>"gray2","edgecolor"=>"none"))
 	axs[1].text(282,13,"ATL",bbox=Dict("facecolor"=>"gray2","edgecolor"=>"none"))
 	axs[1].text(276,1.5,"PCS",bbox=Dict("facecolor"=>"gray2","edgecolor"=>"none"))
-	axs[1].text(285.5,14.4,"(a)",c="w",size=10)
+	axs[1].text(285.5,14.4,"(a)",c="k",size=10)
 	axs[1].text(286.8,14.4,"(b)",c="k",size=10)
 	axs[1].text(286.8,6.5,"(c)",c="k",size=10)
 	axs[1].text(272.5,10,"Liberia",c="k",size=8)
-	axs[1].text(275.2,8.8,"OSA",c="k",size=8)
 	axs[1].text(277,13.2,"San Andres",c="k",size=8)
 	axs[1].text(282,4.8,"Quibdó",c="k",size=8)
 	axs[1].text(278.5,6,"Bahía Solano",c="k",size=8)
@@ -172,29 +172,29 @@ begin
 		grid=true,gridcolor="w",#suptitle="Available Colombia Stations",
 	)
 
-	ix = fig.add_axes([0.617,0.175,0.2,0.36])
+	ix = fig.add_axes([0.635,0.175,0.21,0.36])
 	ix.pcolormesh(
-		lsd_large.lon,lsd_large.lat,lsd_large.lsm',
+		lsd_large.lon,lsd_large.lat,lsd_large.z',
 		levels=lvls,cmap="delta",extend="both",cmap_kw=cmap_dict
 	)
 	ix.pcolormesh(
-		lsd.lon,lsd.lat,lsd.lsm',
+		lsd.lon,lsd.lat,lsd.z',
 		levels=lvls,cmap="delta",extend="both",cmap_kw=cmap_dict
 	)
 	ix.plot(xinset,yinset,lw=0.5,c="k")
-	ix.plot([270,285,285,270,270],[0,0,15,15,0],lw=1.5,c="w")
-	ix.plot([255,300,300,255,255],[-15,-15,30,30,-15],lw=1.5,c="w",linestyle="--")
-	ix.text(260,0,"d02",c="w",size=8)
-	ix.text(290,23,"d01",c="w",size=8)
+	ix.plot([270,285,285,270,270],[0,0,15,15,0],lw=1.5,c="k")
+	ix.plot([255,300,300,255,255],[-15,-15,30,30,-15],lw=1.5,c="k",linestyle="--")
+	ix.text(260,0,"d02",c="k",size=8)
+	ix.text(290,-13,"d01",c="k",size=8)
 	ix.format(
 		xlim=(250,305),ylim=(-20,35),xtickloc="none",ytickloc="none",
 		xlocator=255:15:300,xminorlocator=240:5:315,xticklabels=[],
 		ylocator=-15:15:30,yminorlocator=-30:5:45,yticklabels=[]
 	)
 
-	ix = fig.add_axes([0.617,0.634,0.2,0.30])
+	ix = fig.add_axes([0.635,0.634,0.21,0.30])
 	ix.pcolormesh(
-		lsd.lon,lsd.lat,lsd.lsm',
+		lsd.lon,lsd.lat,lsd.z',
 		levels=lvls,cmap="delta",extend="both",cmap_kw=cmap_dict
 	)
 	ix.plot(xinset,yinset,lw=0.5,c="k")
@@ -212,7 +212,7 @@ begin
 		ylocator=-15:15:30,yminorlocator=-30:5:45,yticklabels=[]
 	)
 
-	fig.colorbar(c,ticklabels=["1e-5","1e-4.5","1e-4","1e-3.5","1e-3","1e-2.5","1e-2","1e-1.5","1e-1","0.25","0.5","0.9","0.95","0.99"],label="Land-Sea Mask")
+	fig.colorbar(c,label="Topographic Height / km")
 	fig.savefig(plotsdir("01b-domain.png"),transparent=false,dpi=300)
 	fig.savefig(plotsdir("Figure1.png"),transparent=false,dpi=300)
 	load(plotsdir("01b-domain.png"))
@@ -230,10 +230,10 @@ end
 # ╟─a6ab92dd-1fae-4a04-b8f3-6782ea67c60b
 # ╟─189e1048-c92d-457e-a30e-f4e523b80afc
 # ╟─32c650df-ccd2-4adf-a3b7-56611fff1b46
-# ╟─60d38ccd-6538-426f-985e-9d782834dce9
+# ╠═60d38ccd-6538-426f-985e-9d782834dce9
 # ╟─c16d89d9-d7ba-4b79-aa7c-8570467333e0
 # ╟─194bb86d-a3bb-4585-ba66-067dd4488189
 # ╟─5d6c3cd6-e406-461d-a226-20022060398d
 # ╟─76e35851-71a0-4b89-98bb-9a82bf34bd34
 # ╟─1a643e58-39c1-4c6b-b340-978056871b6b
-# ╟─d7755534-3565-4011-b6e3-e131991008db
+# ╠═d7755534-3565-4011-b6e3-e131991008db
