23c1ce64b7694d17a5c2672ef4e350e0f40583e3
natgeo-wong <natgeo.wong@outlook.com>
2024-01-02 11:25:35 -0500

    Modifying Figure*.jl files

--- notebooks/Figure1.jl
+++ notebooks/Figure1.jl
@@ -1,88 +1,41 @@
 ### A Pluto.jl notebook ###
-# v0.19.27
+# v0.19.36
 
 using Markdown
 using InteractiveUtils
 
-# ╔═╡ 1d7446ba-f464-44df-89e2-ae2a5726e849
+# ╔═╡ e32a00ee-5f32-47a1-a983-91fb77bc5d18
 begin
 	using Pkg; Pkg.activate()
 	using DrWatson
-	md"Using DrWatson in order to ensure reproducibility between different machines ..."
+	md"Using DrWatson to ensure reproducibility between different machines ..."
 end
 
-# ╔═╡ ab294fae-101f-4587-a2f4-7d72254dd421
+# ╔═╡ bec4e6f2-c2ea-421e-8c57-33e1ef90aa21
 begin
 	@quickactivate "ConvectionIsotopes"
 	using DelimitedFiles
 	using ERA5Reanalysis
+	using GeoRegions
 	using NASAPrecipitation
 	using NCDatasets
-	using PlutoUI
-
+	using Statistics
+	
 	using ImageShow, PNGFiles
 	using PyCall, LaTeXStrings
 	pplt = pyimport("proplot")
 
 	include(srcdir("common.jl"))
-
+	
 	md"Loading modules for the ConvectionIsotopes project..."
 end
 
-# ╔═╡ fa2f8740-f813-11ec-00e1-112e2dfacda7
+# ╔═╡ 2e7c33da-f8b5-11ec-08f2-2581af96575f
 md"
-# Figure 1. Regions of Interest
+# Figure 1. Theoretical $p_\omega$ Schematic
 "
 
-# ╔═╡ ccdf14a6-b200-42db-8455-0ea4eeb5ae2d
-TableOfContents()
-
-# ╔═╡ f252a060-111a-4e86-85dd-46257c258b77
-md"
-### A. Loading GeoRegion Information
-"
-
-# ╔═╡ 74042139-122c-4085-925a-21b421d7f744
-# ╠═╡ show_logs = false
-begin
-	addGeoRegions(srcdir("OTRECRectRegions.txt"),overwrite=true)
-	addGeoRegions(srcdir("OTRECPolyRegions.txt"),overwrite=true)
-	md"Adding all the GeoRegions from predefined files ..."
-end
-
-# ╔═╡ 47127cc3-442e-4fa7-b016-82ca8b6ab87a
-begin
-	PAC = GeoRegion("OTREC_PAC")
-	ATL = GeoRegion("OTREC_ATL")
-	PCS = GeoRegion("OTREC_PCS")
-	SAN = GeoRegion("OTREC_SAN")
-	md"Loading GeoRegions ..."
-end
-
-# ╔═╡ 601f4ad5-3520-4555-af6b-8eb2a299609c
-begin
-	blon_PAC,blat_PAC,slon_PAC,slat_PAC = coordGeoRegion(PAC)
-	blon_ATL,blat_ATL,slon_ATL,slat_ATL = coordGeoRegion(ATL)
-	slon_PCS,slat_PCS = coordGeoRegion(PCS)
-	slon_SAN,slat_SAN = coordGeoRegion(SAN)
-	md"Loading geographic shapes of the GeoRegions ..."
-end
-
-# ╔═╡ a6ab92dd-1fae-4a04-b8f3-6782ea67c60b
-md"
-### B. Loading Datasets and LandSea Masks ...
-"
-
-# ╔═╡ 189e1048-c92d-457e-a30e-f4e523b80afc
-begin
-	infoall = stninfoall()
-	infody  = stninfody()
-	infomo  = stninfomo()
-	infocr  = stninfocostarica()
-	md"Loading station location information ..."
-end
-
-# ╔═╡ 32c650df-ccd2-4adf-a3b7-56611fff1b46
+# ╔═╡ 1cfa1b51-5a64-4945-9e61-82a27900f9de
 begin
 	coast = readdlm(datadir("GLB-i.txt"),comments=true,comment_char='#')
 	x = coast[:,1]
@@ -90,121 +43,199 @@ begin
 	md"Loading coastlines data ..."
 end
 
-# ╔═╡ 60d38ccd-6538-426f-985e-9d782834dce9
-begin
-	xinset = deepcopy(x); xinset[(x.>285).|(x.<270).|(y.>15).|(y.<0)].=NaN
-	yinset = deepcopy(y); yinset[(x.>285).|(x.<270).|(y.>15).|(y.<0)].=NaN
-	md"Filtering out coastlines outside domain ..."
-end
-
-# ╔═╡ c16d89d9-d7ba-4b79-aa7c-8570467333e0
-geo = GeoRegion("OTREC")
-
-# ╔═╡ 194bb86d-a3bb-4585-ba66-067dd4488189
-geo_large = GeoRegion("OTREC_LRG")
-
-# ╔═╡ 5d6c3cd6-e406-461d-a226-20022060398d
-begin
-	lsd = getLandSea(
-		geo,path=datadir(),savelsd=true,returnlsd=true,
-		smooth=true,σlon=30,σlat=30
-	)
-end
-
-# ╔═╡ 76e35851-71a0-4b89-98bb-9a82bf34bd34
-begin
-	lsd_large = getLandSea(
-		geo_large,path=datadir(),savelsd=true,returnlsd=true,
-	)
-end
-
-# ╔═╡ 1a643e58-39c1-4c6b-b340-978056871b6b
+# ╔═╡ 75925166-d1e8-450e-bae6-29affd25d635
 md"
-### C. Plotting Regions of Interest
+### A. Defining Datasets, Variables and Regions
 "
 
-# ╔═╡ d7755534-3565-4011-b6e3-e131991008db
+# ╔═╡ a5487828-a442-4a64-b784-65a7319fc90c
+e5ds = ERA5Monthly(start=Date(1979,1,1),stop=Date(2021,12,31),path=datadir())
+
+# ╔═╡ 9473b30b-787a-495b-bdfe-b386c9995761
+evar = SingleVariable("p_wwgt")
+
+# ╔═╡ c9d1e411-a65b-41f7-949b-cf2e32b2dfb4
+evar_pre = SingleVariable("sp")
+
+# ╔═╡ c7d0ae59-8d78-4946-8348-9fa570197b0b
+egeo = ERA5Region("TRP")
+
+# ╔═╡ cb7c6118-e25b-462a-84a5-751ea0682b52
+elsd = getLandSea(e5ds,egeo,smooth=true,σlon=2,σlat=2)
+
+# ╔═╡ b68195cb-cf2e-4ce4-9999-1d71eacedf6a
+md"
+### B. Loading ERA5 and GPM Datasets
+"
+
+# ╔═╡ acf26a3f-6f2c-4bfa-a0b4-1d0379cc47fe
 begin
-	pplt.close(); fig,axs = pplt.subplots(aspect=27/17,axwidth=5)
+	tmp = zeros(length(elsd.lon),length(elsd.lat),12)
+	wp  = zeros(length(elsd.lon),length(elsd.lat),12)
+	sp  = zeros(length(elsd.lon),length(elsd.lat),12)
+	cnt = zeros(length(elsd.lon),length(elsd.lat),12)
+	for dt in e5ds.start : Year(1) : e5ds.stop
+		wpds = read(e5ds,evar,egeo,dt,quiet=true)
+		spds = read(e5ds,evar_pre,egeo,dt,quiet=true)
+		tmp[:,:,:] .= nomissing(wpds[evar.ID][:,:,:],0) / 100
+		wp[:,:,:]  += tmp
+		cnt[:,:,:] += Int.(.!iszero.(tmp))
+		sp[:,:,:]  += nomissing(spds[evar_pre.ID][:,:,:]) / 100
+		close(wpds)
+		close(spds)
+	end
+	wp = wp ./ cnt; wp_yr = dropdims(mean(wp,dims=3),dims=3)
+	sp = sp ./ 43;  sp_yr = dropdims(mean(sp,dims=3),dims=3)
+	wp_yr[elsd.z.>500] .= NaN
+	md"Loading and filtering w-weighted pressure and sigma data and counts ..."
+end
 
-	lvls = vcat(10. .^(-5:0.5:-1))
-	lvls = vcat(lvls,0.25,0.5,0.9,0.95,0.99)
+# ╔═╡ 459cca84-c2bb-4ebe-8f2d-52e576d62906
+md"
+### C. Calculating Sample Depletion Profiles from WRF
+"
 
-	cmap_dict = Dict("right"=>0.65)
+# ╔═╡ 7c954c2b-bfdc-4efd-a3b4-8038e66e1ee5
+begin
+	wds = NCDataset(datadir("fig2wrfdata.nc"))
+	q   = wds["QVAPOR"][:,:]
+	qh  = wds["HDO_QVAPOR"][:,:]
+	p   = wds["P"][:,:]
+	ps  = wds["PSFC"][:]
+	close(wds)
+	md"Loading the WRF 2D and 3D datasets"
+end
+
+# ╔═╡ 6231902f-897b-4fd1-b6f6-856e11f83168
+begin
+	μq  = dropdims(mean( q[ps.>100000,:],dims=1),dims=1)
+	μqh = dropdims(mean(qh[ps.>100000,:],dims=1),dims=1)
+	μp  = dropdims(mean( p[ps.>100000,:],dims=1),dims=1)
+	μδ  = (μqh ./ μq .- 1) * 1000
+	md"Calculating the average depletion over ocean/lowland regions ..."
+end
+
+# ╔═╡ 00f069bb-0834-4859-94fa-7b0c146e3a11
+md"
+### D. Plotting the climatology
+"
+
+# ╔═╡ 4342521a-6a0a-4d30-a5f3-975514d9006f
+begin
+	pplt.close(); fig,axs = pplt.subplots([
+		[0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+		[0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+		[0,2,2,2,2,2,2,0,0,3,3,3,3,3,3,0],
+		[0,2,2,2,2,2,2,0,0,3,3,3,3,3,3,0],
+		[0,2,2,2,2,2,2,0,0,3,3,3,3,3,3,0],
+		[0,2,2,2,2,2,2,0,0,3,3,3,3,3,3,0],
+		[6,4,4,4,4,4,4,0,0,5,5,5,5,5,5,7],
+		[6,4,4,4,4,4,4,0,0,5,5,5,5,5,5,7],
+		[6,4,4,4,4,4,4,0,0,5,5,5,5,5,5,7],
+	],aspect=7,axwidth=5,wspace=[1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],sharex=0)
 	
-	axs[1].pcolormesh(
-		lsd_large.lon,lsd_large.lat,lsd_large.lsm',alpha=0.6,
-		levels=lvls,cmap="delta",extend="both",cmap_kw=cmap_dict
-	)
-	
-	c = axs[1].pcolormesh(
-		lsd.lon,lsd.lat,lsd.lsm',
-		levels=lvls,cmap="delta",extend="both",cmap_kw=cmap_dict
-	)
-	
-	axs[1].plot(xinset,yinset,lw=0.5,c="k")
-	axs[1].scatter(infomo[:,2],infomo[:,3],zorder=4,c="blue")
-	axs[1].scatter(infody[:,2],infody[:,3],zorder=4,c="pink")
-	axs[1].scatter(infocr[:,2],infocr[:,3],zorder=4,c="red")
-	axs[1].plot([270,285,285,270,270],[0,0,15,15,0],lw=5,c="w")
-	axs[1].plot(slon_PAC,slat_PAC,lw=2,)
-	axs[1].plot(slon_ATL,slat_ATL,lw=2,)
-	axs[1].plot(slon_PCS,slat_PCS,lw=2,)
-	axs[1].plot(slon_SAN,slat_SAN,lw=2,)
-	axs[1].text(271.5,1.5,"PAC",bbox=Dict("facecolor"=>"gray2","edgecolor"=>"none"))
-	axs[1].text(282,13,"ATL",bbox=Dict("facecolor"=>"gray2","edgecolor"=>"none"))
-	axs[1].text(278.3,10.8,"SAN",bbox=Dict("facecolor"=>"gray2","edgecolor"=>"none"))
-	axs[1].text(276,1.5,"PCS",bbox=Dict("facecolor"=>"gray2","edgecolor"=>"none"))
-	axs[1].text(285.5,14.5,"(a)",c="w",size=10)
-	axs[1].text(294,7.6,"(b)",c="k",size=10)
-	axs[1].format(
-		xlim=(269,296),xlocator=255:5:300,xminorlocator=240:2.5:315,
-		ylim=(-1,16),ylocator=-15:5:30,yminorlocator=-20:2.5:35,
-		xlabel=L"Longitude / $\degree$",ylabel=L"Latitude / $\degree$",
-		grid=true,gridcolor="w",#suptitle="Available Colombia Stations",
-	)
+	c1 = axs[1].pcolormesh(elsd.lon,elsd.lat,wp_yr',levels=(9:0.5:15).*50,extend="both",cmap="drywet_r")
 
-	ix = fig.add_axes([0.617,0.175,0.2,0.36])
-	ix.pcolormesh(
-		lsd_large.lon,lsd_large.lat,lsd_large.lsm',
-		levels=lvls,cmap="delta",extend="both",cmap_kw=cmap_dict
-	)
-	ix.pcolormesh(
-		lsd.lon,lsd.lat,lsd.lsm',
-		levels=lvls,cmap="delta",extend="both",cmap_kw=cmap_dict
-	)
-	ix.plot(xinset,yinset,lw=0.5,c="k")
-	ix.plot([270,285,285,270,270],[0,0,15,15,0],lw=1.5,c="w")
-	ix.plot([255,300,300,255,255],[-15,-15,30,30,-15],lw=1.5,c="w",linestyle="--")
-	ix.text(260,0,"d02",c="w",size=8)
-	ix.text(290,23,"d01",c="w",size=8)
-	ix.format(
-		xlim=(250,305),ylim=(-20,35),xtickloc="none",ytickloc="none",
-		xlocator=255:15:300,xminorlocator=240:5:315,xticklabels=[],
-		ylocator=-15:15:30,yminorlocator=-30:5:45,yticklabels=[]
-	)
+	for ii in 1 : 3
+		axs[ii].pcolormesh(elsd.lon,elsd.lat,wp_yr',levels=(9:0.5:15).*50,extend="both",cmap="drywet_r")
+		axs[ii].plot(x,y,c="k",lw=0.5)
+		axs[ii].format(ylabel=L"Latitude / $\degree$",ylocator=-10:10:20,yminorlocator=-10:5:20,xminorlocator=90:5:300)
+	end
 
-	fig.colorbar(c,ticklabels=["1e-5","1e-4.5","1e-4","1e-3.5","1e-3","1e-2.5","1e-2","1e-1.5","1e-1","0.25","0.5","0.9","0.95","0.99"],label="Land-Sea Mask")
-	fig.savefig(plotsdir("Figure1.png"),transparent=false,dpi=300)
-	load(plotsdir("Figure1.png"))
+	axs[1].plot([100,130,130,100,100],[-5,-5,15,15,-5],c="k",linestyle="--")
+	axs[1].plot([260,290,290,260,260],[-5,-5,15,15,-5],c="k",linestyle="--")
+	axs[1].text(133,10,"(b)")
+	axs[1].text(251,-3,"(c)")
+
+	axs[2].plot([0,360],[0,0],c="r",linestyle="--")
+	axs[3].plot([0,360],[5,5],c="r",linestyle="--")
+	
+	axs[1].format(xlim=(90,300),xlocator=60:30:330,ylim=(-10,20),ltitle="(a) Tropical Pacific Basin")
+	axs[2].format(xlim=(100,130),xlocator=100:5:130,ylim=(-5,15),ylocator=-15:5:30,yminorlocator=-15:20,xminorlocator=100:130,ltitle="(b) West Pacific",ultitle="(i)")
+	axs[3].format(xlim=(260,290),xlocator=260:5:290,ylim=(-5,15),ylocator=-15:5:20,yminorlocator=-15:20,xminorlocator=260:290,ltitle="(c) East Pacific",ultitle="(i)")
+
+	c2 = axs[4].contourf(elsd.lon,μp/100,repeat(μδ,outer=(1,1440)),levels=-600:50:-150,cmap="viridis",extend="both",cmap_kw=Dict("left"=>0.2))
+	axs[4].fill_between(elsd.lon,1000,sp_yr[:,121],c="brown")
+	axs[4].plot([0,360],ones(2)*10. ^2.65,c="r",linestyle=":")
+	axs[4].format(xlim=(100,130),xlabel=L"Longitude / $\degree$",xlocator=100:5:130,xminorlocator=100:130,ultitle="(ii)")
+
+	axs[5].contourf(elsd.lon,μp/100,repeat(μδ,outer=(1,1440)),levels=-600:50:-150,cmap="viridis",extend="both",cmap_kw=Dict("left"=>0.2))
+	axs[5].fill_between(elsd.lon,1000,sp_yr[:,101],c="brown")
+	axs[5].plot([0,360],ones(2)*10. ^2.8,c="r",linestyle=":")
+	axs[5].format(xlim=(260,290),xlabel=L"Longitude / $\degree$",xlocator=260:5:290,xminorlocator=260:290,ultitle="(ii)")
+
+	axs[6].plot(0.8sin.((0:0.01:1)*pi),10. .^(LinRange(2.3,3,101)),c="k")
+	axs[6].plot([0,0],10. .^[2,2.3],c="k")
+	axs[6].plot([-5,5],ones(2)*10. ^2.65,c="r",linestyle=":")
+	axs[6].format(ultitle="(iii)")
+	
+	axs[7].plot(0.2sin.((0:0.01:1)*pi).-0.5sin.((0:0.01:1)*2pi),10. .^(LinRange(2.3,3,101)),c="k")
+	axs[7].plot([0,0],10. .^[2,2.3],c="k")
+	axs[7].plot([-5,5],ones(2)*10. ^2.8,c="r",linestyle=":")
+	axs[7].format(ultitle="(iii)")
+
+	for ii in 4 : 5
+		axs[ii].format(ylim=(1000,100),yscale="log",xlocator=0:5:360,ylabel="Pressure / hPa",xlabel=L"Longitude / $\degree$")
+	end
+
+	for ii in 6 : 7
+		axs[ii].format(xlim=(-2,2),xticks=[0],yloc="none",xloc="b",xlabel=L"w")
+	end
+
+	fig.colorbar(c1,locator=400:50:800,minorlocator=(5:0.5:15).*50,label=L"$p_\omega$",length=0.8,rows=(1,6))
+	fig.colorbar(c2,locator=[-600,-150],label=L"$\delta_H$",ticklabels=[L"-",L"+"],rows=(7,9))
+	fig.savefig(plotsdir("fig1-pomegaclimatology.png"),transparent=false,dpi=400)
+	load(plotsdir("fig1-pomegaclimatology.png"))
+end
+
+# ╔═╡ cc7368e3-9591-4bf6-a907-67c585106a8c
+md"
+### E. Loading some 2D Cloud images
+"
+
+# ╔═╡ ee25250d-ddad-4242-9536-d3d0f72e0f85
+begin
+	cds = NCDataset(datadir("cloud.nc"))
+	xx  = cds["x"][:]
+	zz  = cds["z"][:]
+	qt  = cds["QN"][:,:,end] .+ cds["QP"][:,:,end]
+	qt[qt.<0.1] .= NaN
+	close(cds)
+end
+
+# ╔═╡ c07e34e0-01f5-4e0e-afa1-bd3d5f157a87
+begin
+	pplt.close(); ftmp,atmp = pplt.subplots(ncols=2,aspect=2,axwidth=4,sharex=0)
+	
+	atmp[1].contourf(xx[8800:9600]/1000,zz/1000,qt[8800:9600,:]',cmap="greys")
+	atmp[2].contourf(xx[15950:16250]/1000,zz/1000,qt[15950:16250,:]',cmap="greys")
+
+	for ax in atmp
+		ax.format(ylim=(0,12),grid=false,xticks="null",yticks="null")
+	end
+
+	ftmp.savefig(plotsdir("fig1-clouds.png"),transparent=true,dpi=400)
+	load(plotsdir("fig1-clouds.png"))
 end
 
 # ╔═╡ Cell order:
-# ╟─fa2f8740-f813-11ec-00e1-112e2dfacda7
-# ╟─1d7446ba-f464-44df-89e2-ae2a5726e849
-# ╟─ab294fae-101f-4587-a2f4-7d72254dd421
-# ╟─ccdf14a6-b200-42db-8455-0ea4eeb5ae2d
-# ╟─f252a060-111a-4e86-85dd-46257c258b77
-# ╟─74042139-122c-4085-925a-21b421d7f744
-# ╟─47127cc3-442e-4fa7-b016-82ca8b6ab87a
-# ╟─601f4ad5-3520-4555-af6b-8eb2a299609c
-# ╟─a6ab92dd-1fae-4a04-b8f3-6782ea67c60b
-# ╟─189e1048-c92d-457e-a30e-f4e523b80afc
-# ╟─32c650df-ccd2-4adf-a3b7-56611fff1b46
-# ╟─60d38ccd-6538-426f-985e-9d782834dce9
-# ╟─c16d89d9-d7ba-4b79-aa7c-8570467333e0
-# ╟─194bb86d-a3bb-4585-ba66-067dd4488189
-# ╟─5d6c3cd6-e406-461d-a226-20022060398d
-# ╟─76e35851-71a0-4b89-98bb-9a82bf34bd34
-# ╟─1a643e58-39c1-4c6b-b340-978056871b6b
-# ╟─d7755534-3565-4011-b6e3-e131991008db
+# ╟─2e7c33da-f8b5-11ec-08f2-2581af96575f
+# ╟─e32a00ee-5f32-47a1-a983-91fb77bc5d18
+# ╟─bec4e6f2-c2ea-421e-8c57-33e1ef90aa21
+# ╟─1cfa1b51-5a64-4945-9e61-82a27900f9de
+# ╟─75925166-d1e8-450e-bae6-29affd25d635
+# ╟─a5487828-a442-4a64-b784-65a7319fc90c
+# ╟─9473b30b-787a-495b-bdfe-b386c9995761
+# ╟─c9d1e411-a65b-41f7-949b-cf2e32b2dfb4
+# ╟─c7d0ae59-8d78-4946-8348-9fa570197b0b
+# ╟─cb7c6118-e25b-462a-84a5-751ea0682b52
+# ╟─b68195cb-cf2e-4ce4-9999-1d71eacedf6a
+# ╟─acf26a3f-6f2c-4bfa-a0b4-1d0379cc47fe
+# ╟─459cca84-c2bb-4ebe-8f2d-52e576d62906
+# ╟─7c954c2b-bfdc-4efd-a3b4-8038e66e1ee5
+# ╟─6231902f-897b-4fd1-b6f6-856e11f83168
+# ╟─00f069bb-0834-4859-94fa-7b0c146e3a11
+# ╟─4342521a-6a0a-4d30-a5f3-975514d9006f
+# ╟─cc7368e3-9591-4bf6-a907-67c585106a8c
+# ╟─ee25250d-ddad-4242-9536-d3d0f72e0f85
+# ╟─c07e34e0-01f5-4e0e-afa1-bd3d5f157a87
--- notebooks/Figure2.jl
+++ notebooks/Figure2.jl
@@ -1,127 +1,197 @@
 ### A Pluto.jl notebook ###
-# v0.19.27
+# v0.19.36
 
 using Markdown
 using InteractiveUtils
 
-# ╔═╡ b3bc56eb-43a7-4736-bd66-704529911d60
+# ╔═╡ 1d7446ba-f464-44df-89e2-ae2a5726e849
 begin
 	using Pkg; Pkg.activate()
 	using DrWatson
-	md"Using DrWatson to ensure reproducibility between different machines ..."
+	md"Using DrWatson in order to ensure reproducibility between different machines ..."
 end
 
-# ╔═╡ 9071763e-f6ad-4468-ae48-369307a85263
+# ╔═╡ ab294fae-101f-4587-a2f4-7d72254dd421
 begin
 	@quickactivate "ConvectionIsotopes"
-	using Dates
+	using DelimitedFiles
+	using ERA5Reanalysis
+	using NASAPrecipitation
 	using NCDatasets
-	using Printf
-	using Statistics
-	
+	using PlutoUI
+
 	using ImageShow, PNGFiles
 	using PyCall, LaTeXStrings
 	pplt = pyimport("proplot")
 
 	include(srcdir("common.jl"))
-	
+
 	md"Loading modules for the ConvectionIsotopes project..."
 end
 
-# ╔═╡ a8431d46-46fe-11ec-2b8d-e39caffdabec
+# ╔═╡ fa2f8740-f813-11ec-00e1-112e2dfacda7
 md"
-# Figure 2. Station Rainfall and Isotopes
-
-In this notebook, we explicitly compare the rainfall measured by the 3 stations we have, to GPM Rainfall data at the nearest gridpoint corresponding to these stations.
+# Figure 2. OTREC Region of Interest
 "
 
-# ╔═╡ 5b392d8a-d68b-44df-ab57-d0e81aedd668
+# ╔═╡ ccdf14a6-b200-42db-8455-0ea4eeb5ae2d
+TableOfContents()
+
+# ╔═╡ a6ab92dd-1fae-4a04-b8f3-6782ea67c60b
 md"
-### A. Loading the Processed Data
+### A. Loading Datasets and LandSea Masks ...
 "
 
-# ╔═╡ cceac73b-b7d9-4418-b145-d45d384b61b8
+# ╔═╡ 189e1048-c92d-457e-a30e-f4e523b80afc
 begin
-	ds = NCDataset(datadir("processed.nc"))
-	dtvec = ds["time"][:]
-	lon   = ds["longitude"][:]
-	lat   = ds["latitude"][:]
-	prcp  = ds["prcp"][:] 
-	prcpg = ds["prcpg"][:] 
-	prcps = ds["prcps"][:] 
-	δ18Oμ = ds["δ18Oμ"][:] 
-	δ18Oσ = ds["δ18Oσ"][:] 
-	δ2Hμ  = ds["δ2Hμ"][:] 
-	δ2Hσ  = ds["δ2Hσ"][:] 
-	close(ds)
-	md"Loading processed station and GPM rainfall and isotopic data ..."
+	infoall = stninfoall()
+	infody  = stninfody()
+	infomo  = stninfomo()
+	infocr  = stninfocostarica()
+	md"Loading station location information ..."
 end
 
-# ╔═╡ 0374f823-90a0-4dc6-be0e-9449a7fa7202
+# ╔═╡ 32c650df-ccd2-4adf-a3b7-56611fff1b46
 begin
-	arr = [[1,1,1,2,3],[4,4,4,5,6],[7,7,7,8,9]]
-	pplt.close(); fig,axs = pplt.subplots(arr,aspect=3,axwidth=3.5,sharex=0,sharey=0,wspace=[0,0,1,8],hspace=[1,1])
+	coast = readdlm(datadir("GLB-i.txt"),comments=true,comment_char='#')
+	x = coast[:,1]
+	y = coast[:,2]
+	md"Loading coastlines data ..."
+end
 
-	for istn in 1 
-		axs[1].scatter(dtvec,prcpg[:,istn],s=1,c="b")
-		axs[1].scatter(dtvec,prcps[:,istn],s=1,c="r")
-		axs[2].scatter(prcps[:,istn],prcpg[:,istn],s=1,c="k")
-		axs[3].scatter(δ18Oμ[:,istn],δ2Hμ[:,istn],s=1,c="k")
-	end
+# ╔═╡ 60d38ccd-6538-426f-985e-9d782834dce9
+begin
+	xinset = deepcopy(x); xinset[(x.>285).|(x.<270).|(y.>15).|(y.<0)].=NaN
+	yinset = deepcopy(y); yinset[(x.>285).|(x.<270).|(y.>15).|(y.<0)].=NaN
+	md"Filtering out coastlines outside domain ..."
+end
 
-	for istn in 2 : 4
-		axs[4].scatter(dtvec,prcpg[:,istn],s=1,c="b")
-		axs[4].scatter(dtvec,prcps[:,istn],s=1,c="r")
-		axs[5].scatter(prcps[:,istn],prcpg[:,istn],s=1,c="k")
-		axs[6].scatter(δ18Oμ[:,istn],δ2Hμ[:,istn],s=1,c="k")
-	end
+# ╔═╡ c16d89d9-d7ba-4b79-aa7c-8570467333e0
+geo = GeoRegion("OTREC")
 
-	for istn in 5 : 12
-		axs[7].scatter(dtvec,prcpg[:,istn],s=1,c="b")
-		axs[7].scatter(dtvec,prcps[:,istn],s=1,c="r")
-		axs[8].scatter(prcps[:,istn],prcpg[:,istn],s=1,c="k")
-		axs[9].scatter(δ18Oμ[:,istn],δ2Hμ[:,istn],s=1,c="k")
-	end
+# ╔═╡ 194bb86d-a3bb-4585-ba66-067dd4488189
+geo_large = GeoRegion("OTREC_LRG")
 
-	for ii in [1,4,7]
-		axs[ii].format(ylim=(-10,210),xlim=(Date(2019,1),Date(2021,12)))
-	end
+# ╔═╡ 5d6c3cd6-e406-461d-a226-20022060398d
+begin
+	lsd = getLandSea(
+		geo,path=datadir(),savelsd=true,returnlsd=true,
+		smooth=true,σlon=30,σlat=30
+	)
+end
 
-	for ii in [1,2,3,4,5,6]
-		axs[ii].format(xticklabels=[])
-	end
+# ╔═╡ 76e35851-71a0-4b89-98bb-9a82bf34bd34
+begin
+	lsd_large = getLandSea(
+		geo_large,path=datadir(),savelsd=true,returnlsd=true,
+	)
+end
 
-	for ii in [2,5,8]
-		axs[ii].format(xlim=(-10,210),ylim=(-10,210),ytickloc="r")
-	end
+# ╔═╡ 1a643e58-39c1-4c6b-b340-978056871b6b
+md"
+### C. Plotting Regions of Interest
+"
 
-	for ii in [3,6,9]
-		axs[ii].format(xlim=(-20,2),ylim=(-150,25),ytickloc="r")
-	end
+# ╔═╡ d7755534-3565-4011-b6e3-e131991008db
+begin
+	pplt.close(); fig,axs = pplt.subplots(aspect=27/17,axwidth=5)
 
-	for ax in axs
-		ax.format(xminorlocator=[])
-	end
-
-	axs[1].format(ltitle="(a) Time-Series", ultitle="(i) San Andres",)
-	axs[2].format(ltitle="(b) Station vs GPM")
-	axs[3].format(ltitle=L"(c) $\delta^2$H vs $\delta^{18}$O")
-	axs[4].format(ultitle="(ii) Colombia Daily Stations",ylabel=L"Rain Rate / mm day$^{-1}$")
-	axs[5].format(ylabel=L"GPM Rainfall / mm day$^{-1}$")
-	axs[6].format(ylabel=L"$\delta^2$H")
-	axs[7].format(ultitle="(iii) Costa Rica Stations")
-	axs[8].format(xlabel=L"Station Rainfall / mm day$^{-1}$")
-	axs[9].format(xlabel=L"$\delta^{18}$O")
-		
+	lvls = -6 : 6
+	textdict = Dict("fc"=>"grey3","ec"=>"none","alpha"=>0.6)
 	
-	fig.savefig(plotsdir("Figure2.png"),transparent=false,dpi=400)
-	load(plotsdir("Figure2.png"))
+	axs[1].pcolormesh(
+		lsd_large.lon,lsd_large.lat,lsd_large.z'/1000,alpha=0.3,
+		levels=lvls,cmap="delta",extend="both"
+	)
+	
+	c = axs[1].pcolormesh(
+		lsd.lon,lsd.lat,lsd.z'/1000,
+		levels=lvls,cmap="delta",extend="both"
+	)
+	
+	axs[1].plot(xinset,yinset,lw=0.5,c="k")
+	axs[1].plot([275,275,278,278,275],[8,10.5,10.5,8,8],lw=1,c="k",linestyle="--")
+	axs[1].scatter(infody[:,2],infody[:,3],zorder=4,c="pink")
+	axs[1].scatter(infocr[:,2],infocr[:,3],zorder=4,c="red")
+	axs[1].plot([275,288],[10.5,15],lw=1,c="k",linestyle=":")
+	axs[1].plot([278,295],[8,9.03],lw=1,c="k",linestyle=":")
+	axs[1].plot([270,285,285,270,270],[0,0,15,15,0],lw=5,c="k")
+	axs[1].text(285.5,14.5,"(a)",c="k",size=10)
+	axs[1].text(286.8,14.5,"(b)",c="k",size=10)
+	axs[1].text(286.8,6.5,"(c)",c="k",size=10)
+
+	axs[1].text(272.3,10,"Liberia",c="k",size=8,bbox=textdict)
+	axs[1].text(277.5,13,"San Andres",c="k",size=8,bbox=textdict)
+	axs[1].text(278.6,6,"Bahía Solano",c="k",size=8,bbox=textdict)
+	axs[1].text(282,4.75,"Quibdó",c="k",size=8,bbox=textdict)
+	axs[1].text(278.8,3.5,"Buenaventura",c="k",size=8,bbox=textdict)
+	
+	axs[1].format(
+		xlim=(269,296),xlocator=255:5:300,xminorlocator=240:2.5:315,
+		ylim=(-1,16),ylocator=-15:5:30,yminorlocator=-20:2.5:35,
+		xlabel=L"Longitude / $\degree$",ylabel=L"Latitude / $\degree$",
+		grid=true,gridcolor="w",#suptitle="Available Colombia Stations",
+	)
+
+	ix = fig.add_axes([0.635,0.632,0.21,0.30])
+	ix.pcolormesh(
+		lsd.lon,lsd.lat,lsd.z'/1000,
+		levels=lvls,cmap="delta",extend="both"
+	)
+	ix.plot(xinset,yinset,lw=0.5,c="k")
+	ix.scatter(infocr[:,2],infocr[:,3],s=10,zorder=4,c="red")
+
+	ix.text(275.2,10.15,"EEFMB",c="k",size=7,bbox=textdict)
+	ix.text(275.4,9.65,"CGFI",c="k",size=7,bbox=textdict)
+	ix.text(276.1,9.64,"AMDQ",c="k",size=7,bbox=textdict)
+	ix.text(276.1,8.3,"OSA",c="k",size=7,bbox=textdict)
+	ix.text(276.2,10.22,"Bataan",c="k",size=7,bbox=textdict)
+	ix.text(277,10.1,"Limon",c="k",size=7,bbox=textdict)
+	ix.text(277,9.45,"Cahuita",c="k",size=7,bbox=textdict)
+	
+	ix.format(
+		xlim=(275,278),ylim=(8,10.5),xtickloc="none",ytickloc="none",
+		xlocator=255:15:300,xminorlocator=240:5:315,xticklabels=[],
+		ylocator=-15:15:30,yminorlocator=-30:5:45,yticklabels=[]
+	)
+
+	ix = fig.add_axes([0.635,0.175,0.21,0.36])
+	ix.pcolormesh(
+		lsd_large.lon,lsd_large.lat,lsd_large.z'/1000,
+		levels=lvls,cmap="delta",extend="both"
+	)
+	ix.pcolormesh(
+		lsd.lon,lsd.lat,lsd.z'/1000,
+		levels=lvls,cmap="delta",extend="both"
+	)
+	ix.plot(xinset,yinset,lw=0.5,c="k")
+	ix.plot([270,285,285,270,270],[0,0,15,15,0],lw=1.5,c="k")
+	ix.plot([255,300,300,255,255],[-15,-15,30,30,-15],lw=1.5,c="k",linestyle="--")
+	ix.text(260,0,"d02",c="k",size=8)
+	ix.text(290,-12,"d01",c="k",size=8)
+	ix.format(
+		xlim=(250,305),ylim=(-20,35),xtickloc="none",ytickloc="none",
+		xlocator=255:15:300,xminorlocator=240:5:315,xticklabels=[],
+		ylocator=-15:15:30,yminorlocator=-30:5:45,yticklabels=[]
+	)
+
+	fig.colorbar(c,label="Topographic Height/ km")
+	fig.savefig(plotsdir("fig2-otrecdomain.png"),transparent=false,dpi=400)
+	load(plotsdir("fig2-otrecdomain.png"))
 end
 
 # ╔═╡ Cell order:
-# ╟─a8431d46-46fe-11ec-2b8d-e39caffdabec
-# ╟─b3bc56eb-43a7-4736-bd66-704529911d60
-# ╟─9071763e-f6ad-4468-ae48-369307a85263
-# ╟─5b392d8a-d68b-44df-ab57-d0e81aedd668
-# ╟─cceac73b-b7d9-4418-b145-d45d384b61b8
-# ╟─0374f823-90a0-4dc6-be0e-9449a7fa7202
+# ╟─fa2f8740-f813-11ec-00e1-112e2dfacda7
+# ╟─1d7446ba-f464-44df-89e2-ae2a5726e849
+# ╟─ab294fae-101f-4587-a2f4-7d72254dd421
+# ╟─ccdf14a6-b200-42db-8455-0ea4eeb5ae2d
+# ╟─a6ab92dd-1fae-4a04-b8f3-6782ea67c60b
+# ╟─189e1048-c92d-457e-a30e-f4e523b80afc
+# ╟─32c650df-ccd2-4adf-a3b7-56611fff1b46
+# ╟─60d38ccd-6538-426f-985e-9d782834dce9
+# ╟─c16d89d9-d7ba-4b79-aa7c-8570467333e0
+# ╟─194bb86d-a3bb-4585-ba66-067dd4488189
+# ╟─5d6c3cd6-e406-461d-a226-20022060398d
+# ╟─76e35851-71a0-4b89-98bb-9a82bf34bd34
+# ╟─1a643e58-39c1-4c6b-b340-978056871b6b
+# ╠═d7755534-3565-4011-b6e3-e131991008db
