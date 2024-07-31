3a7400b68f2f071717de57fbbf23db6149936795
Nathanael Wong <natgeo.wong@outlook.com>
2024-07-30 20:21:09 -0400

    Updating for wrf2 tilted grids

---
+++ src/wrfsmooth.jl
@@ -0,0 +1,165 @@
+using DrWatson
+using Dates
+using Logging
+using NCDatasets
+using Printf
+using Statistics
+
+function calculatebufferweights(shiftsteps)
+
+    buffer = Int(ceil((shiftsteps-1)/2))
+    weights = ones(buffer*2+1)
+    if buffer >= (shiftsteps/2)
+        weights[1] = 0.5
+        weights[end] = 0.5
+    end
+    weights /= shiftsteps
+    return buffer,weights
+
+end
+
+function wrf3Dsmooth(
+    wvar :: AbstractString;
+	days :: Int = 1
+)
+
+    ds   = NCDataset(datadir("wrf2","3D","$(wvar)-daily.nc"))
+	lon  = ds["longitude"][:,:]; nlon = ds.dim["longitude"]
+    lat  = ds["latitude"][:,:];  nlat = ds.dim["latitude"]
+	nlvl = ds.dim["levels"]
+    ndt  = ds.dim["date"]; start = ds["time"][1]
+	attrib = Dict(ds[wvar].attrib)
+	close(ds)
+
+	buffer,weights = calculatebufferweights(days)
+
+    oarr = zeros(Float32,nlon,nlat,nlvl,1+buffer*2)
+	narr = fill(NaN32,(nlon,nlat,nlvl,ndt))
+	smth = zeros(Float32,1+buffer*2)
+
+	for ii in (1+buffer) : (ndt-buffer)
+
+		@info "$(now()) - ConvectionIsotopes - Extracting $wvar data for Day $ii of $ndt"
+		flush(stderr)
+
+		ids = NCDataset(datadir("wrf2","3D","$(wvar)-daily.nc"))
+		NCDatasets.load!(ids[wvar].var,oarr,:,:,:,ii.+(-buffer:buffer))
+		close(ids)
+
+		for ilvl = 1 : nlvl, ilat = 1 : nlat, ilon = 1 : nlon
+			for ismth = 1 .+ (0 : buffer*2)
+				smth[ismth] = oarr[ilon,ilat,ilvl,ismth] * weights[ismth]
+			end
+			narr[ilon,ilat,ilvl,ii] = Float32(sum(smth))
+		end
+
+	end
+
+	fnc = datadir("wrf2","3D","$wvar-daily-smooth_$(@sprintf("%02d",days))days.nc")
+	if isfile(fnc); rm(fnc,force=true) end
+
+	ds = NCDataset(fnc,"c")
+
+	ds.dim["longitude"] = nlon
+	ds.dim["latitude"]  = nlat
+	ds.dim["levels"]    = nlvl
+	ds.dim["date"]      = ndt
+
+	nclon = defVar(ds,"longitude",Float32,("longitude","latitude"),attrib=Dict(
+		"units"     => "degrees_east",
+        "long_name" => "longitude",
+	))
+
+	nclat = defVar(ds,"latitude",Float32,("longitude","latitude"),attrib=Dict(
+		"units"     => "degrees_north",
+        "long_name" => "latitude",
+	))
+
+	nctime = defVar(ds,"time",Int32,("date",),attrib=Dict(
+		"units"     => "days since $(start) 00:00:00.0",
+        "long_name" => "time",
+        "calendar"  => "gregorian"
+	))
+
+	ncvar = defVar(ds,wvar,Float32,("longitude","latitude","levels","date"),attrib=attrib)
+
+	nclon[:,:] = lon
+	nclat[:,:] = lat
+	nctime.var[:] = collect(0 : (ndt-1))
+	ncvar[:,:,:,:] = narr
+
+	close(ds)
+
+end
+
+function wrf2Dsmooth(
+    wvar :: AbstractString;
+	days :: Int = 1
+)
+
+    ds   = NCDataset(datadir("wrf2","2D","$(wvar)-daily.nc"))
+	lon  = ds["longitude"][:]; nlon = ds.dim["longitude"]
+    lat  = ds["latitude"][:];  nlat = ds.dim["latitude"]
+    ndt  = ds.dim["date"]; start = ds["time"][1]
+	attrib = Dict(ds[wvar].attrib)
+	close(ds)
+
+	buffer,weights = calculatebufferweights(days)
+
+    oarr = zeros(Float32,nlon,nlat,1+buffer*2)
+	narr = fill(NaN32,(nlon,nlat,ndt))
+	smth = zeros(Float32,1+buffer*2)
+
+	for ii in (1+buffer) : (ndt-buffer)
+
+		@info "$(now()) - ConvectionIsotopes - Extracting $wvar data for Day $ii of $ndt"
+		flush(stderr)
+
+		ids = NCDataset(datadir("wrf2","2D","$(wvar)-daily.nc"))
+		NCDatasets.load!(ids[wvar].var,oarr,:,:,ii.+(-buffer:buffer))
+		close(ids)
+
+		for ilat = 1 : nlat, ilon = 1 : nlon
+			for ismth = 1 .+ (0 : buffer*2)
+				smth[ismth] = oarr[ilon,ilat,ismth] * weights[ismth]
+			end
+			narr[ilon,ilat,ii] = Float32(sum(smth))
+		end
+
+	end
+
+	fnc = datadir("wrf2","2D","$wvar-daily-smooth_$(@sprintf("%02d",days))days.nc")
+	if isfile(fnc); rm(fnc,force=true) end
+
+	ds = NCDataset(fnc,"c")
+
+	ds.dim["longitude"] = nlon
+	ds.dim["latitude"]  = nlat
+	ds.dim["date"]      = ndt
+
+	nclon = defVar(ds,"longitude",Float32,("longitude","latitude"),attrib=Dict(
+		"units"     => "degrees_east",
+        "long_name" => "longitude",
+	))
+
+	nclat = defVar(ds,"latitude",Float32,("longitude","latitude"),attrib=Dict(
+		"units"     => "degrees_north",
+        "long_name" => "latitude",
+	))
+
+	nctime = defVar(ds,"time",Int32,("date",),attrib=Dict(
+		"units"     => "days since $(start) 00:00:00.0",
+        "long_name" => "time",
+        "calendar"  => "gregorian"
+	))
+
+	ncvar = defVar(ds,wvar,Float32,("longitude","latitude","date"),attrib=attrib)
+
+	nclon[:,:] = lon
+	nclat[:,:] = lat
+	nctime.var[:] = collect(0 : (ndt-1))
+	ncvar[:,:,:] = narr
+
+	close(ds)
+
+end
---
+++ src/wrfwwgtpre.jl
@@ -0,0 +1,262 @@
+using Dates
+using GeoRegions
+using NCDatasets
+using Printf
+using Statistics
+using Trapz
+
+include(srcdir("backend.jl"))
+
+function wrfwwgtpre(
+    geo  :: GeoRegion;
+    start :: Date,
+    stop  :: Date,
+	days  :: Int = 0
+)
+
+    Rd = 287.053
+    
+    ds   = NCDataset(datadir("wrf2","grid.nc"))
+    lon  = ds["longitude"][:,:]
+    lat  = ds["latitude"][:,:]
+    close(ds)
+
+    ggrd = RegionGrid(geo,lon,lat)
+    apnt = findall(ggrd.mask .== 1)
+    npnt = length(apnt)
+    lon1 = findfirst(ggrd.mask .== 1)[1]; lon2 = findlast(ggrd.mask .== 1)[1]
+    lat1 = findfirst(ggrd.mask .== 1)[2]; lat2 = findlast(ggrd.mask .== 1)[2]
+
+    dtvec = start : Day(1) : stop
+
+    nlon = lon2 - lon1 + 1
+    nlat = lat2 - lat1 + 1
+    nlvl = 50
+    ndt  = length(dtvec)
+
+    warr = zeros(Float32,nlon,nlat,nlvl+1)
+    parr = zeros(Float32,nlon,nlat,nlvl)
+    tarr = zeros(Float32,nlon,nlat,nlvl)
+    psfc = zeros(Float32,nlon,nlat)
+
+    tmp_wmat = zeros(Float32,nlon,nlat,52); tmp_wvec = zeros(Float32,52)
+    tmp_pmat = zeros(Float32,nlon,nlat,52); tmp_pvec = zeros(Float32,52)
+    tmp_ρmat = zeros(Float32,nlon,nlat,52)
+
+    pwgt = zeros(Float32,ndt)
+    σwgt = zeros(Float32,ndt)
+
+    pds  = NCDataset(datadir("wrf2","3D","PB-daily.nc"))
+    pbse = pds["PB"][lon1:lon2,lat1:lat2,:,1]
+    close(pds)
+
+    if iszero(days)
+        wds = NCDataset(datadir("wrf2","3D","W-daily.nc"))
+        pds = NCDataset(datadir("wrf2","3D","P-daily.nc"))
+        tds = NCDataset(datadir("wrf2","3D","T-daily.nc"))
+        sds = NCDataset(datadir("wrf2","2D","PSFC-daily.nc"))
+    else
+        smthstr = "smooth_$(@sprintf("%02d",days))days"
+        wds = NCDataset(datadir("wrf2","3D","W-daily-$smthstr.nc"))
+        pds = NCDataset(datadir("wrf2","3D","P-daily-$smthstr.nc"))
+        tds = NCDataset(datadir("wrf2","3D","T-daily-$smthstr.nc"))
+        sds = NCDataset(datadir("wrf2","2D","PSFC-daily-$smthstr.nc"))
+    end
+
+    for ii in 1 : ndt
+
+        @info "$(now()) - ConvectionIsotopes - Extracting data for Day $ii of $ndt"
+        flush(stderr)
+
+        NCDatasets.load!(wds["W"].var,warr,lon1:lon2,lat1:lat2,:,ii)
+        NCDatasets.load!(pds["P"].var,parr,lon1:lon2,lat1:lat2,:,ii)
+        NCDatasets.load!(tds["T"].var,tarr,lon1:lon2,lat1:lat2,:,ii)
+        NCDatasets.load!(sds["PSFC"].var,psfc,lon1:lon2,lat1:lat2,ii)
+
+        for ilvl = 1 : nlvl, ilat = 1 : nlat, ilon = 1 : nlon
+            parr[ilon,ilat,ilvl] += pbse[ilon,ilat,ilvl]
+            tarr[ilon,ilat,ilvl] += 290
+            tarr[ilon,ilat,ilvl]  = tarr[ilon,ilat,ilvl] * (100000 / parr[ilon,ilat,ilvl]) ^ (287/1004)
+        end
+
+        for ilat = 1 : nlat, ilon = 1 : nlon
+            
+            tmp_pmat[ilon,ilat,1] = psfc[ilon,ilat]
+
+            for ilvl = 1 : nlvl
+                tmp_wmat[ilon,ilat,ilvl+1]  = (warr[ilon,ilat,ilvl] + warr[ilon,ilat,ilvl+1]) / 2
+                tmp_ρmat[ilon,ilat,ilvl+1]  =  parr[ilon,ilat,ilvl] / Rd / tarr[ilon,ilat,ilvl]
+                tmp_wmat[ilon,ilat,ilvl+1] *= (tmp_ρmat[ilon,ilat,ilvl+1] * -9.81)
+                tmp_pmat[ilon,ilat,ilvl+1]  = parr[ilon,ilat,ilvl]
+            end
+
+        end
+
+        tmp_wvec = reverse(dropdims(mean(tmp_wmat,dims=(1,2)),dims=(1,2)))
+        tmp_pvec = reverse(dropdims(mean(tmp_pmat,dims=(1,2)),dims=(1,2)))
+
+        pwgt[ii] = trapz(tmp_pvec,tmp_wvec.*tmp_pvec) / trapz(tmp_pvec,tmp_wvec)
+        
+        σwgt[ii] = pwgt[ii] / mean(psfc) 
+
+    end
+
+    close(wds)
+    close(pds)
+    close(tds)
+    close(sds)
+
+    mkpath(datadir("wrf2","processed"))
+    if iszero(days)
+        fnc = datadir("wrf2","processed","$(geo.ID)-p_wwgt-daily.nc")
+    else
+        smthstr = "smooth_$(@sprintf("%02d",days))days"
+        fnc = datadir("wrf2","processed","$(geo.ID)-p_wwgt-daily-$smthstr.nc")
+    end
+
+    if isfile(fnc); rm(fnc,force=true) end
+
+    ds = NCDataset(fnc,"c")
+    ds.dim["date"]      = ndt
+
+    nctime = defVar(ds,"time",Int32,("date",),attrib=Dict(
+        "units"     => "days since $(start) 00:00:00.0",
+        "long_name" => "time",
+        "calendar"  => "gregorian"
+    ))
+
+    ncpwgt = defVar(ds,"p_wwgt",Float32,("date",),attrib=Dict(
+        "long_name" => "column_mean_lagrangian_tendency_of_air_pressure",
+        "full_name" => "Vertical Wind Weighted Column Pressure",
+        "units"     => "Pa",
+    ))
+
+    ncσwgt = defVar(ds,"σ_wwgt",Float32,("date",),attrib=Dict(
+        "long_name" => "column_mean_lagrangian_tendency_of_sigma",
+        "full_name" => "Vertical Wind Weighted Column Sigma",
+        "units"     => "0-1",
+    ))
+
+    nctime.var[:] = collect(0 : (ndt-1))
+    ncpwgt[:] = pwgt
+    ncσwgt[:] = σwgt
+
+    close(ds)
+
+end
+
+function wrfwwgtpre()
+
+    Rd = 287.053
+    
+    ds   = NCDataset(datadir("wrf2","grid.nc"))
+    lon  = ds["longitude"][:,:]
+    lat  = ds["latitude"][:,:]
+    close(ds)
+
+    nlon = size(lon,1)
+    nlat = size(lat,2)
+    nlvl = 50
+
+    warr = zeros(Float32,nlon,nlat,nlvl+1)
+    parr = zeros(Float32,nlon,nlat,nlvl)
+    tarr = zeros(Float32,nlon,nlat,nlvl)
+    psfc = zeros(Float32,nlon,nlat)
+
+    tmp_wvec = zeros(Float32,52)
+    tmp_pvec = zeros(Float32,52)
+    tmp_ρvec = zeros(Float32,52)
+
+    pwgt = zeros(Float32,nlon,nlat)
+    σwgt = zeros(Float32,nlon,nlat)
+
+    pds  = NCDataset(datadir("wrf2","3D","PB-daily.nc"))
+    pbse = pds["PB"][:,:,:,1]
+    close(pds)
+
+    wds = NCDataset(datadir("wrf2","3D","W-daily.nc"))
+    pds = NCDataset(datadir("wrf2","3D","P-daily.nc"))
+    tds = NCDataset(datadir("wrf2","3D","T-daily.nc"))
+    sds = NCDataset(datadir("wrf2","2D","PSFC-daily.nc"))
+    rds = NCDataset(datadir("wrf2","2D","RAINNC-daily.nc"))
+
+    warr = dropdims(mean(wds["W"][:,:,:,:],dims=4),dims=4)
+    parr = dropdims(mean(pds["P"][:,:,:,:],dims=4),dims=4)
+    tarr = dropdims(mean(tds["T"][:,:,:,:],dims=4),dims=4)
+    psfc = dropdims(mean(sds["PSFC"][:,:,:],dims=3),dims=3)
+    rain = dropdims(mean(rds["RAINNC"][:,:,:],dims=3),dims=3)
+
+    close(wds)
+    close(pds)
+    close(tds)
+    close(sds)
+    close(rds)
+
+    for ilvl = 1 : nlvl, ilat = 1 : nlat, ilon = 1 : nlon
+        parr[ilon,ilat,ilvl] += pbse[ilon,ilat,ilvl]
+        tarr[ilon,ilat,ilvl] += 290
+        tarr[ilon,ilat,ilvl]  = tarr[ilon,ilat,ilvl] * (100000 / parr[ilon,ilat,ilvl]) ^ (287/1004)
+    end        
+
+    for ilat = 1 : nlat, ilon = 1 : nlon
+        
+        tmp_pvec[1] = psfc[ilon,ilat]
+
+        for ilvl = 1 : nlvl
+            tmp_ρvec[ilvl+1]  =  parr[ilon,ilat,ilvl] / Rd / tarr[ilon,ilat,ilvl]
+            tmp_wvec[ilvl+1]  = (warr[ilon,ilat,ilvl] + warr[ilon,ilat,ilvl+1]) / 2
+            tmp_wvec[ilvl+1] *= (tmp_ρvec[ilvl+1] * -9.81)
+            tmp_pvec[ilvl+1]  =  parr[ilon,ilat,ilvl]
+        end
+
+        calc = trapz(tmp_pvec,tmp_wvec.*tmp_pvec) / trapz(tmp_pvec,tmp_wvec)
+        if (calc > 0) & (calc < psfc[ilon,ilat]) & (rain[ilon,ilat] > 5)
+            pwgt[ilon,ilat] = calc
+            σwgt[ilon,ilat] = calc / psfc[ilon,ilat]
+        else
+            pwgt[ilon,ilat] = NaN32
+            σwgt[ilon,ilat] = NaN32
+        end
+
+    end
+
+    mkpath(datadir("wrf2","processed"))
+    fnc = datadir("wrf2","processed","p_wwgt-wrf.nc")
+    if isfile(fnc); rm(fnc,force=true) end
+
+    ds = NCDataset(fnc,"c")
+    ds.dim["longitude"] = nlon
+    ds.dim["latitude"]  = nlat
+
+    ncpwgt = defVar(ds,"p_wwgt",Float32,("longitude","latitude",),attrib=Dict(
+        "long_name" => "column_mean_lagrangian_tendency_of_air_pressure",
+        "full_name" => "Vertical Wind Weighted Column Pressure",
+        "units"     => "Pa",
+    ))
+
+    ncσwgt = defVar(ds,"σ_wwgt",Float32,("longitude","latitude",),attrib=Dict(
+        "long_name" => "column_mean_lagrangian_tendency_of_sigma",
+        "full_name" => "Vertical Wind Weighted Column Sigma",
+        "units"     => "0-1",
+    ))
+
+    ncrain = defVar(ds,"RAINNC",Float32,("longitude","latitude",),attrib=Dict(
+        "long_name" => "total_precipitation",
+        "full_name" => "Total Precipitation",
+        "units"     => "mm",
+    ))
+
+    ncpsfc = defVar(ds,"PSFC",Float32,("longitude","latitude",),attrib=Dict(
+        "long_name" => "surface_pressure",
+        "full_name" => "Surface Pressure",
+        "units"     => "Pa",
+    ))
+
+    ncpwgt[:,:] = pwgt
+    ncσwgt[:,:] = σwgt
+    ncrain[:,:] = rain
+    ncpsfc[:,:] = psfc
+
+    close(ds)
+
+end
