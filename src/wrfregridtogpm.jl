9e20bcfb40f9a994b8129834f6c915b15551fccc
Nathanael Wong <natgeo.wong@outlook.com>
2025-01-31 02:22:28 -0500

    Adding regridding to gpm grid for easier comparison of rainfall to observations

---
+++ src/wrfregridtogpm.jl
@@ -0,0 +1,118 @@
+using Dates
+using NASAPrecipitation
+using NCDatasets
+using Statistics
+
+function wrfnewregridgpm2D(
+	wvar  :: AbstractString;
+	start :: Date,
+	stop  :: Date,
+)
+
+	dtbegstr = Dates.format(start,dateformat"yyyymmdd")
+	dtbegend = Dates.format(stop,dateformat"yyyymmdd")
+	timestr = "$(dtbegstr)_$(dtbegend)"
+    
+    ds   = NCDataset(datadir("wrf3","grid.nc"))
+    wlon = ds["longitude"][:,:]; nx,ny = size(wlon)
+    wlat = ds["latitude"][:,:]
+    close(ds)
+
+	ds  = NCDataset(datadir("wrf3","raw","$start.nc"))
+	attrib = Dict(ds[wvar].attrib)
+	close(ds)
+
+    dtvec = start : Day(1) : stop
+    ndt  = length(dtvec)
+
+	npd  = IMERGDummy(path=datadir())
+    geo  = GeoRegion("OTREC",path=srcdir())
+	lsd  = getLandSea(npd,geo)
+	nlon = length(lsd.lon)
+	nlat = length(lsd.lat)
+
+	ipnt_lon = zeros(Int,nx,ny)
+	ipnt_lat = zeros(Int,nx,ny)
+	for ilat = 1 : ny, ilon = 1 : nx
+		ipnt_lon[ilon,ilat] = argmin(abs.(wlon[ilon,ilat].-lsd.lon.+360))
+		ipnt_lat[ilon,ilat] = argmin(abs.(wlat[ilon,ilat].-lsd.lat))
+	end
+
+	tmp1 = zeros(Float32,nx,ny,24)
+	tmp2 = zeros(Float32,nx,ny)
+	tmp3 = zeros(Float32,nx,ny,24)
+	ndata = zeros(nlon,nlat,24,ndt) * NaN
+	for idt = 1 : ndt
+
+		@info "$(now()) - ConvectionIsotopes - Regridding $wvar data for Day $idt of $ndt"
+		flush(stderr)
+		fnc1 = datadir("wrf3","raw","$(dtvec[idt]).nc")
+		
+        if isfile(fnc1)
+
+            ds1 = NCDataset(fnc1)
+
+            if ds1.dim["Time"] == 24
+                
+                fnc2 = datadir("wrf3","raw","$(dtvec[idt]+Day(1))-e.nc")
+                if !isfile(fnc2)
+                    fnc2 = datadir("wrf3","raw","$(dtvec[idt]+Day(1)).nc")
+                else
+                    @info "$(now()) - ConvectionIsotopes - Tail end"
+                end
+                ds2 = NCDataset(fnc2)
+
+				NCDatasets.load!(ds1[wvar].var,tmp1,:,:,:)
+                NCDatasets.load!(ds2[wvar].var,tmp2,:,:,1)
+                tmp3 .= cat(tmp1[:,:,2:end],tmp2,dims=3) .- tmp1
+
+				for ihr = 1 : 24, ilat = 1 : nlat, ilon = 1 : nlon
+					ind = (ipnt_lon.==ilon).&(ipnt_lat.==ilat)
+					idata = @view tmp3[:,:,ihr,ind][ind]
+					ndata[ilon,ilat,ihr,idt] = mean(idata[.!isnan.(idata)])
+				end
+
+			end
+
+		end
+
+	end
+
+	close(wds)
+
+	if !isdir(datadir("wrf3","regridded")); mkpath(datadir("wrf3","regridded")) end
+	fnc = datadir("wrf3","regridded","gpm-RAINNC-$timestr.nc")
+	if isfile(fnc); rm(fnc,force=true) end
+
+	ds = NCDataset(fnc,"c")
+
+	ds.dim["longitude"] = nlon
+	ds.dim["latitude"]  = nlat
+	ds.dim["date"]      = ndt * 24
+
+	nclon = defVar(ds,"longitude",Float32,("longitude",),attrib=Dict(
+		"units"     => "degrees_east",
+        "long_name" => "longitude",
+	))
+
+	nclat = defVar(ds,"latitude",Float32,("latitude",),attrib=Dict(
+		"units"     => "degrees_north",
+        "long_name" => "latitude",
+	))
+
+	nctime = defVar(ds,"time",Int32,("date",),attrib=Dict(
+		"units"     => "days since $(start) 00:00:00.0",
+        "long_name" => "time",
+        "calendar"  => "gregorian"
+	))
+
+	ncvar = defVar(ds,wvar,Float32,("longitude","latitude","date"),attrib=attrib)
+
+	nclon[:] = lsd.lon
+	nclat[:] = lsd.lat
+	nctime.var[:] = collect(0 : (ndt*24 -1)) .+ 0.5
+	ncvar[:,:,:] = ndata
+
+	close(ds)
+
+end
