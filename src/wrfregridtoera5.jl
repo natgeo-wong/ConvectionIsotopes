61301862f7bddd8e01c11a7551467870f0291c49
Nathanael Wong <natgeo.wong@outlook.com>
2025-03-04 18:21:50 -0500

    Do a regridding of daily and smoothed data.

--- src/wrfregridtoera5.jl
+++ src/wrfregridtoera5.jl
@@ -124,3 +124,100 @@ function wrfnewregridera2D(
 	close(ds)
 
 end
+
+function wrfregriddaily2D(
+	wvar  :: AbstractString;
+	start :: Date,
+	stop  :: Date,
+	days  :: Int = 0,
+)
+
+	dtbegstr = Dates.format(start,dateformat"yyyymmdd")
+	dtbegend = Dates.format(stop,dateformat"yyyymmdd")
+	timestr = "$(dtbegstr)_$(dtbegend)"
+	smthstr = "smooth_$(@sprintf("%02d",days))days"
+    
+    ds   = NCDataset(datadir("wrf3","grid.nc"))
+    wlon = ds["longitude"][:,:]; nx,ny = size(wlon)
+    wlat = ds["latitude"][:,:]
+    close(ds)
+
+	if iszero(days)
+        vds = NCDataset(datadir("wrf3","2D","$wvar-daily-$timestr.nc"))
+    else
+        vds = NCDataset(datadir("wrf3","2D","$wvar-daily-$timestr-$smthstr.nc"))
+    end
+
+	odata = vds[wvar][:,:,:]
+	close(vds)
+
+    dtvec = start : Day(1) : stop
+    ndt  = length(dtvec)
+
+	e5ds = ERA5Dummy(path=datadir())
+    egeo = ERA5Region("OTREC_wrf_d02",path=srcdir())
+	elsd = getLandSea(e5ds,egeo)
+	nlon = length(elsd.lon)
+	nlat = length(elsd.lat)
+
+	ipnt_lon = zeros(Int,nx,ny)
+	ipnt_lat = zeros(Int,nx,ny)
+	for ilat = 1 : ny, ilon = 1 : nx
+		ipnt_lon[ilon,ilat] = argmin(abs.(wlon[ilon,ilat].-elsd.lon))
+		ipnt_lat[ilon,ilat] = argmin(abs.(wlat[ilon,ilat].-elsd.lat))
+	end
+
+	ndata = zeros(nlon,nlat,ndt) * NaN
+
+	Threads.@threads for idx in 1:(ndt * nlat * nlon)
+		# Compute indices
+		idt = div(idx - 1, (nlat * nlon)) + 1
+		ilat = div(mod(idx - 1, (nlat * nlon)), nlon) + 1
+		ilon = mod(idx - 1, nlon) + 1
+		
+		ind = (ipnt_lon.==ilon).&(ipnt_lat.==ilat)
+		idata = @view odata[:,:,idt]
+		idata = @view idata[ind]
+		ndata[ilon,ilat,idt] = mean(idata[.!isnan.(idata)])
+	end
+
+	if !isdir(datadir("wrf3","regridded")); mkpath(datadir("wrf3","regridded")) end
+	if iszero(days)
+		fnc = datadir("wrf3","regridded","era-$(wvar)-$timestr.nc")
+    else
+        fnc = datadir("wrf3","processed","era-$(wvar)-$timestr-$smthstr.nc")
+    end
+	if isfile(fnc); rm(fnc,force=true) end
+
+	ds = NCDataset(fnc,"c")
+
+	ds.dim["longitude"] = nlon
+	ds.dim["latitude"]  = nlat
+	ds.dim["date"]      = ndt * 24
+
+	nclon = defVar(ds,"longitude",Float32,("longitude",),attrib=Dict(
+		"units"     => "degrees_east",
+        "long_name" => "longitude",
+	))
+
+	nclat = defVar(ds,"latitude",Float32,("latitude",),attrib=Dict(
+		"units"     => "degrees_north",
+        "long_name" => "latitude",
+	))
+
+	nctime = defVar(ds,"time",Float64,("date",),attrib=Dict(
+		"units"     => "days since $(start) 00:00:00.0",
+        "long_name" => "time",
+        "calendar"  => "gregorian"
+	))
+
+	ncvar = defVar(ds,wvar,Float32,("longitude","latitude","date"),attrib=attrib)
+
+	nclon[:] = elsd.lon
+	nclat[:] = elsd.lat
+	nctime.var[:] = collect(0 : (ndt*24 -1)) .+ 0.5
+	ncvar[:,:,:] = ndata
+
+	close(ds)
+
+end
